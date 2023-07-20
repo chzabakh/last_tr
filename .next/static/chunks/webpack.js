432735192aaabc5011a99292fb3b6ae86090c761
Oumaima Fisaoui <oufisaou@e3r6p8.1337.ma>
2023-07-20 12:48:46 +0100

    back button

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9cd20867d3dccbda"; }
+/******/ 		__webpack_require__.h = function() { return "e7e43bfa1cc6aa98"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Sections/createChannel.tsx
+++ src/components/Sections/createChannel.tsx
@@ -2,12 +2,14 @@
 import React, { useState } from 'react';
 import PublicChannel from '../channels/publicChannel';
 import PrivateChannel from '../channels/privateChannel';
+import BrowseChannel from "./browseChannel";
 import ProtectedChannel from '../channels/protectedChannel';
-
+import  Channels  from './channels';
 
 const CreateChannel = () => {
 
     const [active, setActive] = useState("null");
+    const [back, setback] = useState(false)
 
     function activatePublic()
     {
@@ -24,45 +26,59 @@ const CreateChannel = () => {
             setActive("protected")
     }
 
+    function handleback()
+    {
+        setback(true)
+    }
   return (
     <>
-    
-        <div className="flex p-3 flex-col px-20 border-2 items-center gap-2 h-full  w-[77%] border-opacity-30 border-violet-400 bg-opacity-5 bg-gradient-to-l from-[rgba(255,255,255,0.20)] bg-transparent bg-blur-md backdrop-filter backdrop-blur-md rounded-[30px]">
-            <button className='bg-black/20 self-start w-[100px] border-4 rounded-full'>Back</button>
-        <h1 className='self-start my-10'>Create a channel:</h1>
-        <div className='bg-black/20 w-full  h-[70%]  flex justify-between items-center px-5'>
-            <div className='flex flex-col justify-between gap-10'>
-                <div>Select type of channel:</div>
-                <div className="flex flex-col gap-1 flex-1">
-                    <div>
-                    <input id="default-radio-1" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onClick={activatePublic}></input>
-                    <label htmlFor="default-radio-1" className="ml-2 text-sm font-medium text-white dark:text-gray-300">Public channel</label>
-                    </div>
-                    <div>
-                    <input id="default-radio-1" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onClick={activatePrivate}></input>
-                    <label htmlFor="default-radio-1" className="ml-2 text-sm font-medium text-white dark:text-gray-300">Private channel</label>
-                    </div>
+        {
+            back === true ? <Channels /> : (
 
-                    <div>
-                    <input id="default-radio-1" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onClick={activateProtected}></input>
-                    <label htmlFor="default-radio-1" className="ml-2 text-sm font-medium text-white dark:text-gray-300">Protected channel</label>
+                <>
+                {
+                    <div className="flex p-3 flex-col px-20 border-2 items-center gap-2 h-full  w-[77%] border-opacity-30 border-violet-400 bg-opacity-5 bg-gradient-to-l from-[rgba(255,255,255,0.20)] bg-transparent bg-blur-md backdrop-filter backdrop-blur-md rounded-[30px]">
+                    <button className='bg-black/20 self-start w-[100px] border-4 rounded-full' onClick={handleback}>Back</button>
+                <h1 className='self-start my-10'>Create a channel:</h1>
+                <div className='bg-black/20 w-full  h-[70%]  flex justify-between items-center px-5'>
+                    <div className='flex flex-col justify-between gap-10'>
+                        <div>Select type of channel:</div>
+                        <div className="flex flex-col gap-1 flex-1">
+                            <div>
+                            <input id="default-radio-1" type="radio" value="public" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onClick={activatePublic}></input>
+                            <label htmlFor="default-radio-1" className="ml-2 text-sm font-medium text-white dark:text-gray-300">Public channel</label>
+                            </div>
+                            <div>
+                            <input id="default-radio-1" type="radio" value="private" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onClick={activatePrivate}></input>
+                            <label htmlFor="default-radio-1" className="ml-2 text-sm font-medium text-white dark:text-gray-300">Private channel</label>
+                            </div>
+
+                            <div>
+                            <input id="default-radio-1" type="radio" value="protected" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onClick={activateProtected}></input>
+                            <label htmlFor="default-radio-1" className="ml-2 text-sm font-medium text-white dark:text-gray-300">Protected channel</label>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='bg-black/20 p-5 flex flex-col h-[70%] gap-10  w-[50%]'>
+                        <div>
+                            <h1>Channel Information:</h1>
+                        </div>
+                        <div className='bg-black/20 p-5 h-full flex flex-col justify-evenly'>
+                        {active && active === "public" && <PublicChannel />}
+                        {active && active === "private" && <PrivateChannel />}
+                        {active && active === "protected" && <ProtectedChannel />}
+                        {!active && <PublicChannel />}
+                        </div>
                     </div>
                 </div>
+                </div>
+                }
+                </>
+                
+            )
+        }
     
-            </div>
-            <div className='bg-black/20 p-5 flex flex-col h-[70%] gap-10  w-[50%]'>
-                <div>
-                    <h1>Channel Information:</h1>
-                </div>
-                <div className='bg-black/20 p-5 h-full flex flex-col justify-evenly'>
-                {active && active === "public" && <PublicChannel />}
-                {active && active === "private" && <PrivateChannel />}
-                {active && active === "protected" && <ProtectedChannel />}
-                {!active && <PublicChannel />}
-                </div>
-            </div>
-        </div>
-        </div>
     </>
   )
 }
