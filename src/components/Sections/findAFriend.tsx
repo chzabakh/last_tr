40417eb3183ab4d68f5ebbcb15e4c004863f3f0b
chzabakh <anonymous_systeme@hotmail.com>
2023-07-23 21:28:23 +0100

    working on add friend

--- src/components/Sections/findAFriend.tsx
+++ src/components/Sections/findAFriend.tsx
@@ -8,13 +8,24 @@ type Friend = {
   id: string;
   nickname: string;
   email: string;
-}
+};
 
 const FindAFriend = () => {
   const [item, setItem] = useState("6");
   const [input, setInput] = useState("");
-  const [friend, setFriend] = useState<Friend>({avatarUrl: "pnull", id: "null", nickname: "null", email: "null"});
+  const [friend, setFriend] = useState<Friend>({
+    avatarUrl: "null",
+    id: "null",
+    nickname: "null",
+    email: "null",
+  });
   const { login, accessToken } = useAuth();
+  const config = {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+    recipientUserName: "player2",
+  };
 
   console.log("please work [" + accessToken + "]");
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
@@ -23,6 +34,7 @@ const FindAFriend = () => {
 
   const getUser = async (username: string) => {
     try {
+      console.log("input: [" + input + "]");
       const res = await axios.get(
         `http://localhost:9000/users/${input}/profile`,
         {
@@ -31,14 +43,23 @@ const FindAFriend = () => {
           },
         }
       );
-      console.log(res);
-      setFriend(res.data);
-      console.log(friend.avatarUrl);
+      console.log("39", res);
+      if (res.data == "") {
+        setFriend({
+          avatarUrl: "null",
+          id: "null",
+          nickname: "null",
+          email: "null",
+        });
+      } else {
+        setFriend(res.data);
+        console.log("49", friend.avatarUrl);
+      }
     } catch (err) {
       if (err instanceof AxiosError) {
-        console.log(err.response?.data.message);
+        console.log("53" + err.response?.data.message);
       } else {
-        console.log("Unexpected error", err);
+        console.log("55" + "Unexpected error", err);
       }
     }
   };
@@ -53,30 +74,43 @@ const FindAFriend = () => {
 
   const addUser = async (username: string) => {
     try {
-      const me = await axios.get(
-        `http://localhost:9000/users/me`,
+
+
+      // const me = await axios
+      //   .get(`http://localhost:9000/users/me`, {
+      //     headers: {
+      //       Authorization: `Bearer ${localStorage.getItem("token")}`,
+      //     },
+      //   })
+      //   .catch((err) => {
+      //     console.log("shit", err.message);
+      //   });
+      // console.log("hellllllll", me?.data.nickname);
+
+
+      const sendFriendReq = await axios.post(
+        `http://localhost:9000/users/player1/send-friend-request`,
         {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
+          config,
         }
       );
-      const add = await axios.post(
-        `http://localhost:9000/users/me`,
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        }
-      );
-      console.log(me.data.nickname);
+      // const add = await axios.post(
+      //   `http://localhost:9000/users/me`,
+      //   {
+      //     headers: {
+      //       Authorization: `Bearer ${localStorage.getItem("token")}`,
+      //     },
+      //   }
+      // );
+      // console.log(me.data.nickname);
+
       // setFriend(res.data);
       // console.log(friend.avatarUrl);
     } catch (err) {
       if (err instanceof AxiosError) {
-        console.log(err.response?.data.message);
+        console.log("89" + err.response?.data.message);
       } else {
-        console.log("Unexpected error", err);
+        console.log("91" + "Unexpected error", err);
       }
     }
   };
@@ -110,7 +144,7 @@ const FindAFriend = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
             >
-              <circle cx="11" cy="11" r="8" />{" "}
+              <circle cx="11" cy="11" r="8" />
               <line x1="21" y1="21" x2="16.65" y2="16.65" />
             </svg>
           </button>
@@ -119,16 +153,20 @@ const FindAFriend = () => {
           <div className="flex flex-col border-2  border-opacity-30 mx-auto w-[70%] min-h-[300px] h-[70%] border-violet-400 bg-opacity-5 bg-black bg-gradient-to-l from-[rgba(255,255,255,0.27)] bg-blur-md backdrop-filter backdrop-blur-md p-4 rounded-[30px]">
             <Image
               className="object-cover h-12 w-12 sm:h-20 sm:w-20 md:h-w-30 md:w-30 xl:h-40 xl:w-40 2xl:h-60 2xl:w-60 mx-auto rounded-[20px]"
+              // src={`/avatars/player2.png`}
               src={`/avatars/${friend.avatarUrl}`}
               alt="pdp"
               height={80}
               width={80}
             />
-            <p className="font-serif text-center py-5 text-xl">{friend.nickname}</p>
+            <p className="font-serif text-center py-5 text-xl">
+              {friend.nickname}
+            </p>
             <div className="w-full  absolute bottom-10 flex justify-evenly left-0">
               <button
-              onClick={(e) => handleAdd(e, friend.nickname)} 
-              className="text-[#38FFF3] hover:bg-white hover:bg-opacity-10 w-1/3 text-xs border-2  border-opacity-30 border-violet-400 bg-opacity-5 bg-black bg-gradient-to-l from-[rgba(255,255,255,0.15)] bg-blur-md backdrop-filter backdrop-blur-md p-4 rounded-[30px]">
+                onClick={(e) => handleAdd(e, friend.nickname)}
+                className="text-[#38FFF3] hover:bg-white hover:bg-opacity-10 w-1/3 text-xs border-2  border-opacity-30 border-violet-400 bg-opacity-5 bg-black bg-gradient-to-l from-[rgba(255,255,255,0.15)] bg-blur-md backdrop-filter backdrop-blur-md p-4 rounded-[30px]"
+              >
                 Add friend
               </button>
               <button
@@ -139,7 +177,12 @@ const FindAFriend = () => {
               </button>
             </div>
           </div>
-        ) : null}
+        ) : (
+          <div>
+            {" "}
+            <p>User not found</p>
+          </div>
+        )}
       </div>
       {item == "9" ? (
         <>
