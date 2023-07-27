f25406343bc098dd910fa540e3e2425732df459b
chzabakh <anonymous_systeme@hotmail.com>
2023-07-27 16:33:32 +0100

    reseting

--- jsx.d.ts
+++
@@ -1,8 +0,0 @@
-import 'react';
-
-declare module 'react' {
-  interface IntrinsicElements {
-    Image: React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>;
-    Link: React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
-  }
-}
