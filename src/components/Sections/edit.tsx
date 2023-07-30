3cc8fc243cbc1f19dbfe62eb9904f56a9f26fd71
chzabakh <anonymous_systeme@hotmail.com>
2023-07-30 17:38:21 +0100

    fixed friends

--- src/components/Sections/edit.tsx
+++ src/components/Sections/edit.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect } from 'react'
-import TwoFac from './TwoFac'
+import TwoFac from './twoFac'
 import { useState, ChangeEvent} from 'react';
 import Image from 'next/image';
 import axios from 'axios';
