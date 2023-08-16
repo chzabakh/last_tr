7b93822eecfa1d265b9dc100816e34dfe8a8c2c8
oumaimafisaoui <oumaimafisaoui@gmail.com>
2023-08-17 00:18:53 +0100

    E

---
+++ src/components/Sections/chatRoom.tsx
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import React, {useEffect, useState} from 'react'
+
+const ChatRoom = () => {
+
+    useEffect(() =>
+    {
+        getFriends();
+
+    }, [])
+
+    async function getFriends()
+    {
+        try
+        {
+            const token = Cookies.get('token')
+            const headers = { Authorization: `Bearer ${token}` };
+            const res = await axios.post('http://localhost:9000/users/friendlist', { headers });
+            console.log(res.data);
+            
+        }
+        catch(e)
+        {
+            if(axios.isAxiosError(e))
+            {
+                if(e.request)
+                    console.log("No response received!", e.request);
+                else if(e.response)
+                    console.log("Error status: ", e.response?.status);
+                    console.log("Error data: ", e.response?.data);
+            }
+            else
+            {
+                console.log("Error: ", e);
+            }
+        }
+    }
+  return (
+    <div>ChatRoom</div>
+  )
+}
+
+export default ChatRoom
