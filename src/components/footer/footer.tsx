5010697083703a3f50b87961327c5e564e2cfe4a
oumaimafisaoui <oumaimafisaoui@gmail.com>
2023-08-23 16:08:52 +0100

    footer done

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,8 +5,8 @@ import images from '../../../public/images.png'
 const Footer = () => {
   return (
     // text-xs w-full flex justify-between right-4 flex-end absolute opacity-[0.5]
-    <div className="flex justify-between w-full">
-        <div>@2023 Made with &lt;3 for Trancendance.</div>
+    <div className="flex justify-between w-full text-xs">
+        <div className=' opacity-[0.5]' >@2023 Made with &lt;3 for Trancendance.</div>
         <Image src={images} width={20} height={20} alt="" />
     </div>
   )
