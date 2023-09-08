1dfd4fce9b5c8e76979a84bfcc3fb3443db29459
chzabakh <anonymous_systeme@hotmail.com>
2023-09-08 21:59:35 +0100

    leaderboard and history finished

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,15 +1,43 @@
 import DashboardLayout from "@/components/Layout/dashboardLayout";
 import { User } from "@/components/Sections/findAFriend";
-import Avatar from "@/components/avatar";
 import axios, { AxiosError } from "axios";
 import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import Score from "@/tools/score";
+import Avatar from "@/components/avatar";
+
+export interface Scoreuser {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  email: string;
+  nickname: string;
+  hash: string;
+  TwofaAutSecret: null | string;
+  TwofaAutEnabled: boolean;
+  FirstLogin: boolean;
+  avatarUrl: string;
+  state: string;
+  provider: string;
+  friendStatus: string;
+  matchesAsWinner: {
+    id: number;
+    createdAt: string;
+    updatedAt: string;
+    player1Id: number;
+    player2Id: number;
+    winnerId: number;
+    player1Score: number;
+    player2Score: number;
+  }[];
+  wins: number;
+}
 
 const Leaderboard = () => {
   const [me, setMe] = useState<User>();
-  const [users, setUsers] = useState();
+  const [users, setUsers] = useState<Scoreuser[]>();
+  const [rank, setRank] = useState(0);
   useEffect(() => {
     const getUser = async () => {
       try {
@@ -29,11 +57,14 @@ const Leaderboard = () => {
     };
     const getUsers = async () => {
       try {
-        const users = await axios.get(`http://localhost:9000/users/all-users`, {
-          headers: {
-            Authorization: `Bearer ${Cookies.get("token")}`,
-          },
-        });
+        const users = await axios.get(
+          `http://localhost:9000/game/leaderboard`,
+          {
+            headers: {
+              Authorization: `Bearer ${Cookies.get("token")}`,
+            },
+          }
+        );
         setUsers(users.data);
       } catch (err) {
         if (err instanceof AxiosError) {
@@ -47,38 +78,79 @@ const Leaderboard = () => {
     getUser();
     getUsers();
   }, []);
-console.log("theusers:",users);
+
+  useEffect(() => {
+    if (users && me) {
+      const index = users.findIndex((user) => user.nickname === me?.nickname);
+      setRank(index + 1);
+    }
+  }, [users, me]);
+
+  console.log("theusers:", users);
   return (
     <>
       {/* <DashboardLayout> */}
       <div className="my-20 h-[80%] gap-3 flex flex-col w-full mx-[2rem]  border-2 border-opacity-30 border-violet-400 bg-opacity-20 bg-black bg-blur-md backdrop-filter backdrop-blur-md p-4 rounded-[30px]">
         <div className="flex border-2 h-[10em] flex-auto border-opacity-30 border-violet-400 bg-opacity-5 bg-gradient-to-l from-[rgba(114,39,175,0.2)] bg-blur-md backdrop-filter backdrop-blur-md p-4 rounded-[30px]">
-          <div className="flex space-x-3">
-            <Image className="rounded-md w-auto" src="/place.png" width={90} height={80} alt="asd" />
-            <div className="flex flex-col my-auto">
-              <p>{me?.nickname}</p>
-              <p>#12</p>
+          <div className="flex space-x-6 w-full">
+            <div className="my-auto w-[35%] flex flex-row items-center space-x-4">
+              {me?.provider === "email" ? (
+                <Avatar currentUser={me} />
+              ) : (
+                <Image
+                  className="w-15 h-15 sm:w-20 sm:h-20 rounded-md"
+                  src={me?.avatarUrl!}
+                  width={100}
+                  height={100}
+                  alt="asd"
+                />
+              )}
+              <p className="text-sm sm:text-lg md:text-3xl">{me?.nickname}</p>
+            </div>
+            <div className="flex flex-row my-auto w-[65%] justify-end sm:justify-center">
+              <p className="text-sm sm:text-lg md:text-3xl">Your Rank:</p>
+              <p className="text-sm sm:text-lg md:text-3xl">#{rank}</p>
             </div>
           </div>
         </div>
-        <div className="overflow-y-auto border-2 flex-auto border-opacity-30 border-violet-400 bg-opacity-5 bg-gradient-to-l from-[rgba(165,89,228,0.2)] bg-blur-md backdrop-filter backdrop-blur-md p-4 rounded-[30px]">
-          <Score />
-          <Score />
-          <Score />
-          <Score />
-          <Score />
-          <Score />
-          <Score />
-          <Score />
-          <Score />
-          <Score />
-          <Score />
-          <Score />
-          <Score />
-          <Score />
-          <Score />
-          <Score />
-          <Score />
+        <div className="w-full h-full overflow-y-hidden border-2 flex-auto border-opacity-30 border-violet-400 bg-opacity-5 bg-gradient-to-l from-[rgba(165,89,228,0.2)] bg-blur-md backdrop-filter backdrop-blur-md py-4 rounded-[30px]">
+          <div className="flex justify-around w-full h-[4em] py-4">
+            <p>User</p>
+            <p className="">Score</p>
+            <p>Rank</p>
+          </div>
+          <div
+            style={{ height: "calc(100% - 4em)" }}
+            className="w-full h-full overflow-y-scroll"
+          >
+            {users?.length! > 0 ? (
+              users!.map((user, index) => (
+                <Score key={index} user={user} rank={index + 1} />
+              ))
+            ) : (
+              <p className="h-full flex justify-center items-center md:text-lg lg:text-2xl">
+                no games available!
+              </p>
+            )}
+            {users?.length! > 0 ? (
+              users!.map((user, index) => (
+                <Score key={index} user={user} rank={index + 1} />
+              ))
+            ) : (
+              <p className="h-full flex justify-center items-center md:text-lg lg:text-2xl">
+                no games available!
+              </p>
+            )}
+            {users?.length! > 0 ? (
+              users!.map((user, index) => (
+                <Score key={index} user={user} rank={index + 1} />
+              ))
+            ) : (
+              <p className="h-full flex justify-center items-center md:text-lg lg:text-2xl">
+                no games available!
+              </p>
+            )}
+          </div>
         </div>
       </div>
       {/* </DashboardLayout> */}
--- src/tools/score.tsx
+++ src/tools/score.tsx
@@ -1,18 +1,32 @@
+import Avatar from "@/components/avatar";
+import { Scoreuser } from "@/pages/leaderboard";
 import Image from "next/image";
-const Score = () => {
+
+interface ScoreProps {
+  user: Scoreuser;
+  rank: number;
+}
+
+const Score: React.FC<ScoreProps> = ({ user, rank }) => {
   return (
     <>
-      <div className="my-2 flex items-center justify-around w-full ">
-          <p>#1</p>
-          <Image
-            className="w-5 sm:w-20 rounded-md"
-            src="/place.png"
-            width={100}
-            height={100}
-            alt="asd"
-          />
-        <p className="text-xs lg:text-lg">username</p>
-        <p className="text-xs lg:text-lg">matchs won:</p>
+      <div className="my-3 flex items-center justify-around w-full space-x-3">
+        <div className="flex flex-row items-center space-x-2">
+          {user.provider === "email" ? (
+            <Avatar currentUser={user} />
+          ) : (
+            <Image
+              className="rounded-full max-w-[50px] max-h-[50px]"
+              src={user.avatarUrl}
+              width={100}
+              height={100}
+              alt="asd"
+            />
+          )}
+          <p className="text-xs lg:text-lg">{user.nickname}</p>
+        </div>
+        <p className="text-xs lg:text-lg">{user.wins}</p>
+        <p>#{rank}</p>
       </div>
     </>
   );
